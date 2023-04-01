e0d703ec7bb364d7f48ce0baf2a9a926e12667a8
Showman_Das <Showmandas3216@gmail.com>
2023-04-01 23:25:52 +0600

    slider done

---
+++ src/components/Slider/SlideContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Slider.css'
+
+const SlideContent = (props) => {
+    console.log(props.slideinfo);
+    const{img,title,id,description}=props.slideinfo;
+    return (
+        <div className="carousel-item active">
+      <img src={img} className="d-block w-100 h-50 img-fluid" id='slideImg' alt="..."/>
+      <div className="text-dark carousel-caption bg-transparent w-50 m-auto" id='slidecontent'>
+        <h4 className='fw-bold'>{title}</h4>
+        <p className='fw-semibold'>{description}</p>
+      </div>
+    </div>
+    );
+};
+
+export default SlideContent;
